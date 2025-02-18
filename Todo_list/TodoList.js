9a2d63d6ae9b83e1d996f8e29eea3a9f172da0d8
haribo-119 <kangwyz1@gmail.com>
2025-02-19 00:16:55 +0900

    List CSS 수정, JS 입력시 엔터키 동작되도록 수정

--- Todo_list/TodoList.js
+++ Todo_list/TodoList.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded',function(){
     const addBtn = document.getElementById('add_btn');
+    const input = document.getElementById('input');
 
-    if(addBtn){
-        addBtn.addEventListener('click',function(){
+    if(addBtn){   
+        addBtn.addEventListener('click',addTask);
+    } else {
+        console.error('Button with ID "add_btn" not found.');
+    }
+    if(input){
+        input.addEventListener('keypress', function(e) {
+            if(e.key === 'Enter') {
+                addTask();
+            }
+        });
+    }
+
+
+    function addTask(){
             const input = document.getElementById('input');
             const taskList = document.querySelector('.list_box ul');
 
@@ -11,17 +25,8 @@ document.addEventListener('DOMContentLoaded',function(){
                 li.textContent = input.value;
                 taskList.appendChild(li);
                 input.value = '';
-
-
-                const mainBox = document.querySelector('.main_box');
-                const height = 130 + (taskList.children.length * 20);
-                mainBox.style.height =`${height}px`; 
-
             } 
-
-        });
-    } else {
-        console.error('Button with ID "add_btn" not found.');
     }
+
 
 });
