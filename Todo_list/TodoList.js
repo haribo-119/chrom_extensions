257cb3423a0e32fd6c63f8fefe22f640dec1b97f
haribo-119 <kangwyz1@gmail.com>
2025-02-20 00:47:10 +0900

    add 버튼 task 추가 , task 불러오기

--- Todo_list/TodoList.js
+++ Todo_list/TodoList.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded',function(){
     const addBtn = document.getElementById('add_btn');
     const input = document.getElementById('input');
+    const taskList = document.querySelector('.list_box ul'); 
+
+    loadTasks();
 
     if(addBtn){   
         addBtn.addEventListener('click',addTask);
@@ -15,17 +18,40 @@ document.addEventListener('DOMContentLoaded',function(){
         });
     }
 
-
-    function addTask(){
-            const input = document.getElementById('input');
-            const taskList = document.querySelector('.list_box ul');
-
-            if(input.value.trim() !== ""){
+    //불러오기
+    function loadTasks() {
+        chrome.storage.sync.get(['tasks'], function(result) {
+            const tasks = result.tasks || [];
+            taskList.innerHTML = ''; 
+            tasks.forEach(task => {
                 const li = document.createElement('li');
-                li.textContent = input.value;
+                li.textContent = task;
+                
+                
                 taskList.appendChild(li);
-                input.value = '';
-            } 
+            });
+        });
+    }
+
+
+ 
+    //추가
+    function addTask() {
+        const input = document.getElementById('input');
+        const taskText = input.value.trim();
+
+        if(taskText !== ""){
+            chrome.storage.sync.get(['tasks'], function(result) {
+                const tasks = result.tasks || [];
+                tasks.push(taskText);
+                
+                
+                chrome.storage.sync.set({tasks: tasks}, function() {
+                    loadTasks(); 
+                    input.value = ''; 
+                });
+            });
+        }
     }
 
 
